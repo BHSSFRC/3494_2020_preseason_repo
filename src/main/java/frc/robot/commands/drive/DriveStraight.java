d8dbcae1386e96eb43e50054c07d1bb43b9d0215
unknown <brownkat6@mccsc.edu>
2019-09-09 17:42:58 -0400

    DriveStraight extends Command

--- src/main/java/frc/robot/commands/drive/DriveStraight.java
+++ src/main/java/frc/robot/commands/drive/DriveStraight.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.drive;
 
+import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.OI;
 import frc.robot.RobotMap;
 import frc.robot.sensors.NavX;
@@ -7,7 +8,9 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.QuadTimer;
 import frc.robot.util.SynchronousPIDF;
 
-public class DriveStraight extends Drive {
+import static frc.robot.commands.drive.Drive.setCamera;
+
+public class DriveStraight extends Command {
 
     /**
      * Indicator of which side of the robot is the "front" for operator driving. Set to true iff the driver has
@@ -24,7 +27,6 @@ public class DriveStraight extends Drive {
 
         //takes same PID constants as DriveAntitipPD does. if necessary will add new set of constants
         pidController = new SynchronousPIDF(RobotMap.DRIVE.KP, RobotMap.DRIVE.KI, RobotMap.DRIVE.KD);
-        pidController.setSetpoint(NavX.getInstance().getFusedHeading() * Math.PI / 180);
 
         pidController.setInputRange(0, 2 * Math.PI);
         pidController.setContinuous(true);
@@ -33,6 +35,11 @@ public class DriveStraight extends Drive {
         this.m_timer = new QuadTimer();
     }
 
+    @Override
+    protected void initialize(){
+        pidController.setSetpoint(NavX.getInstance().getFusedHeading() * Math.PI / 180);
+        this.m_timer.start();
+    }
 
     @Override
     protected void execute() {
@@ -43,9 +50,7 @@ public class DriveStraight extends Drive {
 
         stickSpeeds[0] += pidOutput;
         stickSpeeds[1] -= pidOutput;
-        stickSpeeds = this.normalize(stickSpeeds);
-
-
+        
         if (!sideFlipped) {
             Drivetrain.getInstance().tankDrive(stickSpeeds[0], stickSpeeds[1]);
         } else {
